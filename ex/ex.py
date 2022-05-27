f57e0699a839d8d8e93e8eab0f830e4563f3e317
lucasplr <lucasriffel55@gmail.com>
2022-05-26 23:18:38 -0300

    update

---
+++ ex/ex.py
@@ -0,0 +1,3 @@
+a = open('texto.txt', 'r')
+b = a.read()
+print(b)
--- exe.py
+++ exe.py
@@ -1,10 +1,10 @@
 from itertools import count
 
 
-fhand = open('romeo.txt')
+fhand = open('texto.txt')
 counts = dict()
 for line in fhand:
-    words = line>split()
+    words = line.split()
     for word in words:
         counts[word] = counts.get(word, 0) + 1
 
