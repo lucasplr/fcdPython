8dc8e8ee3fb7baf1cde600b7aa2ed24e1e8bb54d
lucasplr <lucasriffel55@gmail.com>
2022-05-30 01:19:11 -0300

    update

--- ArithmeticFormater/arithmetic_arranger.py
+++ ArithmeticFormater/arithmetic_arranger.py
@@ -1,16 +1,18 @@
 import re
-def arrithmetic_arranger(problems, solve=False):
+def arithmetic_arranger(problems, solve=False):
     #["32 + 698", "3801 - 2", "45 + 43", "123 + 49"]
     #into
     #" 3 3001  45    123\n+  855  -  2  +  43  +  49\n
     # --- ----  --   ----    ---     ---  ----   ---- "
-    fisrt = ''
+    if len(problems) >= 5:
+        return 'Error: Too many problems.'
+    first = ''
     second = ''
     lines = ''
     sumx = ''
     string = ''
     for problem in problems:
-        if (re.search('[^\s0-9.+-'), problem):
+        if (re.search('[^\s0-9.+-]', problem)):
             if (re.search('[/]', problem)) or re.search('[*]', problem):
                 return 'ERROR: Operator must be "+" or "-".'
             return 'ERROR: Numbers must only contain digits.'
@@ -19,24 +21,36 @@ def arrithmetic_arranger(problems, solve=False):
         operator = problem.split(' ')[1]
         secondNumber = problem.split(' ')[2]
 
-        if len(firstNumber) >= 5 or (secondNumber) >= 5:
+        if (len(firstNumber)) >= 5 or (len(secondNumber)) >= 5:
             return 'Error: Numbers cannot be more than four digits.'
         sum = ''
-        if (operator = '+'):
+        if operator == '+':
             sum = str(int(firstNumber) + int(secondNumber)) 
-        elif (operator = '-'):
+        elif operator == '-':
             sum = str(int(firstNumber) - int(secondNumber))
 
-        length = max(len(firstNumber), len(secondNumber))
+        length = max(len(firstNumber), len(secondNumber)) + 2
         top = str(firstNumber).rjust(length)
         bottom = operator + str(secondNumber).rjust(length - 1)
         line = ''
         res = str(sum).rjust(length)
         for s in range(length):
-            line *= '-'
-        
+            line += '-'
+        if problem != problems[-1]:
+            first += top + '    '
+            second += bottom + '    '
+            lines += line + '    '
+            sumx += res + '    '
+        else:
+            first += top
+            second += bottom
+            lines += line
+            sumx += res 
+    if solve:
+        string = first + '\n' + second + '\n' + lines + '\n' + sumx
+    else:
+        string = first + '\n' + second + '\n' + lines
+    return string
 
-
-return arranged_problems
 
 
--- ArithmeticFormater/main.py
+++ ArithmeticFormater/main.py
@@ -2,7 +2,7 @@ from arithmetic_arranger import arithmetic_arranger
 from unittest import main
 
 
-arithmetic_arranger(["32 + 698", "3801 - 2", "45 + 43", "123 + 49"])
+print(arithmetic_arranger(["32 + 698", "3801 - 2", "45 + 43", "123 + 49"], True))
 
 #run unit tests automatically
-main(module='test module', exit=False)
+main(module='test_module', exit=False)
