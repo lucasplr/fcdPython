f1821e10a4cdb4e8bc31bf83205e1336aef1e382
lucasplr <lucasriffel55@gmail.com>
2022-06-01 23:03:17 -0300

    update

--- budgetApp/budget.py
+++ budgetApp/budget.py
@@ -1,1 +1,56 @@
+def chart(categories):
+    return None
+
+
+
+class Category:
+
+
+    def __init__(self, name):
+        self.name = name
+        self.ledger = list()
+    def deposit(self, amount, description=''):
+        """
+        A deposit methot that acceps an amount description.
+        """
+        self.ledger.append({'amount': amount, 'description': description})
+    def withdraw(self, amount, description=''):
+        """
+        A withdraw method.
+        """
+        if(self.check_funds(amount)):
+            self.ledger.append({'amount': -amount, 'description': description})
+            return True
+        return False
+    def get_balance(self):
+        """
+        A get_balance methot that returns the current balance of the budget.
+        """
+        total_cash = 0
+        for item in self.ledger:
+            total_cash+=item['amount']
+        return total_cash
+    def transfer(self, amount, category=''):
+        """
+        A transfer method that accepts an amount and another budget category as arguments.
+        """
+        if(self.check_funds(amount)):
+            self.withdraw(amount, 'Transfer to', category.name)
+            return True
+        return False
+    def check_funds(self, amount):
+        if(self.get_balance() >= amount):
+            return True
+        return False
+        
+
+
+
+
+
+
+
+
+
+
 
--- budgetApp/main.py
+++ budgetApp/main.py
@@ -1,1 +1,19 @@
-from budget import *
+import budget
+from budget import chart
+from unittest import main
+
+food = budget.Category('Food')
+food.deposit(1000, 'initial_deposit')
+food.withdraw(10.15, 'groceries')
+food.withdraw(15.89, 'restaurant and more food for dessert')
+print(food.get_balance())
+clothing = budget.Category('clothing')
+food.transfer(50, clothing)
+clothing.withdraw(25.55)
+clothing.withdraw(100)
+auto = budget.Category('Auto')
+auto.deposit(1000, 'initial deposit')
+auto.withdraw(15)
+
+main(module='test_module', exit=False)
+
