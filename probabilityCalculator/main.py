4e4810faf9ce210d8c9203836ac6333b4e5991a2
lucasplr <lucasriffel55@gmail.com>
2022-06-02 23:54:40 -0300

    update

---
+++ probabilityCalculator/main.py
@@ -0,0 +1,8 @@
+import prob_calculator
+from prob_calculator import Hat
+
+
+hat1 = Hat(orange=1, blue=2, green=6)
+
+
+print(hat1)
--- probabilityCalculator/prob_calculator.py
+++ probabilityCalculator/prob_calculator.py
@@ -1,4 +1,12 @@
+import copy
+import random
 
 
 class Hat:
     
+    def __init__(self, **kwargs):
+        print(kwargs)
+        content = []
+        for c in range(0, len(kwargs)):
+            content.append(kwargs)
+        print(content)
