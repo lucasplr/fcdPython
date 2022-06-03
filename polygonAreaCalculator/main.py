3f0c072b804097e4047ec1061ea2c0dcf2fb7e87
lucasplr <lucasriffel55@gmail.com>
2022-06-02 21:44:37 -0300

    update

---
+++ polygonAreaCalculator/main.py
@@ -0,0 +1,10 @@
+import shape_calculator
+from unittest import main
+
+rect = shape_calculator.Rectangle(10, 5)
+print(rect.get_area())
+rect.set_height(3)
+print(rect.get_perimeter())
+print(rect)
+print(rect.get_picture())
+print
--- polygonAreaCalculator/shape_calculator.py
+++ polygonAreaCalculator/shape_calculator.py
@@ -1,26 +1,49 @@
 def shape_calculator():
-    return None
+    return Rectangle
 
 
-class Category:
+class Rectangle:
+
+    def set_width(self, newW):
+        self.width = newW
+
+    def set_height(self, newH):
+        self.height = newH
 
     def __init__(self, width=5, height=10):
         self.width = width
-        self.heigth = height
+        self.height = height
 
-    def get_area(self, width, height):
-        area = width * height
+    def get_area(self):
+        area = self.width * self.height
         return area
     
 
-    def get_perimeter(self, width, heigth):
-        perimeter = 2 * width + 2 * heigth
+    def get_perimeter(self):
+        perimeter = 2 * self.width + 2 * self.height
         return perimeter
 
-    def get_diagonal(self, width, heigth):
-        diagonal = width**2 + heigth**2
+    def get_diagonal(self):
+        diagonal = self.width**2 + self.height**2
         return diagonal
     
-    def get_picture(self, width, height):
-        if width > 50 or height > 50:
+    def get_picture(self):
+        if self.width > 50 or self.height > 50:
             return 'Too big for picture.'
+        else:
+            tot = self.height
+            res = ''
+            while tot > 0:
+                res += f'*'*self.width + '\n'
+                tot-=1
+            return res
+    
+    def get_amount_inside(self, shape):
+        return int(self.get_area() / shape.get_area())
+
+
+
+class Square(Rectangle):
+    def __init__(self, side=9):
+        self.width = side
+        self.height = side
